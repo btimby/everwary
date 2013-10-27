afdc90176b4a1b1281d2d6ab07872f8999d1434a
Ben Timby <btimby@gmail.com>
2013-10-26 22:20:40 -0400

    Enable console logging.

--- everwary/everwary/settings.py
+++ everwary/everwary/settings.py
@@ -163,6 +163,11 @@ LOGGING = {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
         }
     },
     'loggers': {
