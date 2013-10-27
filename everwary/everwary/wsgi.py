b8ad2581305edbf8cb99081bc60a24d584df8f57
Ben Timby <btimby@gmail.com>
2013-10-26 21:18:48 -0400

    Fixed name.

--- everwary/everwary/wsgi.py
+++ everwary/everwary/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for camden project.
+WSGI config for everwary project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
