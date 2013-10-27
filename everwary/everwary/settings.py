c172880b9371c977604933dd0da7419ec1d0a270
Ben Timby <btimby@gmail.com>
2013-10-26 21:56:05 -0400

    Set static files collection point.

--- everwary/everwary/settings.py
+++ everwary/everwary/settings.py
@@ -73,7 +73,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
